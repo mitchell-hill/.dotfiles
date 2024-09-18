92f5cbefe15203f9019b2aca71987518a02e9d37
Mitchell-Hill <159163711+mitchell-hill@users.noreply.github.com>
2024-09-18 09:58:07 -0400

    update

--- setup_script.sh
+++ setup_script.sh
@@ -2,25 +2,30 @@
 # Created by: Mitchell Hill
 # # Created on: Sept 2024
 # # AWS Debian Linux .dotfiles ans setup script
-#
-# sudo apt update
-# sudo apt upgrade -y
-#
-# sudo apt remove w3m -y
-# sudo apt install gh -y
-# sudo apt install stow 
-# 
-# mkdir temp
-# cd temp
-# sudo apt get install ninja-build gettext cmake unzip curl
-# git clone https://github.com/neovim/neovim
-# cd neovim && make CMAKE_BUILD_TYPE=RelWIthDebInfo
-# sudo make install
-# cd ..
-# cd ..
-# sudo rm -r ./temp
 
-# curl -fsSL https://bun.sh/install | bash
-# sudo apt install -y default-jdk
-#
-# cp -r ./scripts/ ~/
+sudo apt update
+sudo apt upgrade -y
+
+# common programs
+sudo apt remove w3m -y
+sudo apt install git -y
+sudo apt install gh -y
+sudo apt install stow -y
+sudo apt install tree -y
+
+# install NeoVim from source
+mkdir temp
+cd temp || exit
+sudo apt-get install ninja-build gettext cmake unzip curl -y
+git clone https://github.com/neovim/neovim
+cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+cd .. || exit
+cd .. || exit
+sudo rm -r ./temp
+
+# install languages, bun and Java
+sudo apt install -y default-jdk
+curl -fsSL https://bun.sh/install | bash
+# shellcheck source=/dev/null
+source ~/.bashrc
